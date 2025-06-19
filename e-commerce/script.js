a4e9f6dc18959e5c04d291953afdbd260bc5ba52
Amam Raushan <amanraushan0@gmail.com>
2025-06-19 16:43:19 +0530

    add script.js file to it

--- e-commerce/script.js
+++ e-commerce/script.js
@@ -1,10 +1,10 @@
 // Mobile Menu Toggle
-const bar = document.getElementById('bar');
+const bar1 = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
-if (bar) {
-    bar.addEventListener('click', () => {
+if (bar1) {
+    bar1.addEventListener('click', () => {
         nav.classList.add('active');
     });
 }
